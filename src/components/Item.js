02f1554ba1b8e981902df4984ffb90b77476061d
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-08-22 03:56:28 -0600

    sets up the add to cart remove from cart component so that it works

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  let [isOnCart, setIsOnCart] = useState(false);
+
+  function handleAddToRemoveFromCartListener(event)
+  {
+    event.target.parentNode.className = (isOnCart ? "" : "in-cart");
+    setIsOnCart((myoncart) => !myoncart);
+  }
+
   return (
-    <li className="">
+    <li className={isOnCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddToRemoveFromCartListener}>
+        {(!isOnCart) ? "Add to Cart" : "Remove From Cart"}
+      </button>
     </li>
   );
 }
